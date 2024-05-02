9324eeb69e97a5d8c036db39d8e45b409110a3db
Julien BARRA <Julien.barra89100@gmail.coml>
2024-05-02 15:28:24 +0200

    structure login ok

---
+++ frontend/components/ModalLogin.js
@@ -0,0 +1,48 @@
+import Modal from "react-modal";
+import styles from "../styles/ModalLogin.module.css";
+
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+    backgroundColor: "#141D27",
+  },
+};
+
+function ModalLogin(props) {
+  let subtitle;
+
+  function afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    subtitle.style.color = "#f00";
+  }
+
+  return (
+    <div>
+      <Modal
+        isOpen={props.modalIsOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={props.closeModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+      >
+        <h2 ref={(_subtitle) => (subtitle = _subtitle)}>Hello</h2>
+        <button onClick={props.closeModal}>close</button>
+        <div>I am a modal</div>
+        <form>
+          <input />
+          <button>tab navigation</button>
+          <button>stays</button>
+          <button>inside</button>
+          <button>the modal</button>
+        </form>
+      </Modal>
+    </div>
+  );
+}
+
+export default ModalLogin;
